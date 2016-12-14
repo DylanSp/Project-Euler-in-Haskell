6bf8b1a99001b2aa5c17976c8903679a76768e03
dylansp <dylan.richard.sprague@gmail.com>
2016-12-14 18:46:21 +0000

    Add sumDigits to Utils

--- utils/Utils.hs
+++ utils/Utils.hs
@@ -2,6 +2,8 @@ module Utils
 ( olMinus
 , primesToM
 , pythTriples
+, factorial
+, choose
 ) where
 
 --set difference on ordered lists
@@ -21,3 +23,18 @@ primesToM m = 2 : sieve [3,5..m]
        | otherwise = p : sieve (xs `olMinus` [p*p, p*p+2*p..])
        
 pythTriples = [(m * m - n * n, 2 * m * n, m * m + n * n) | m <- [1..], n <- [1..m], m /= n ]
+
+
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+choose :: Integer -> Integer -> Integer
+choose n 0 = 1
+choose 0 k = 0
+choose n k = choose (n-1) (k-1) * n `div` k 
+
+sumDigits :: Integer -> Integer
+sumDigits n
+    | n < 10    = n
+    | otherwise = n `mod` 10 + sumDigits (n `div` 10)
